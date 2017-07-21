c260e96b196134f14ab844f0347c1a80a1c3e91c
Matias Juntunen <matias.juntunen@gmail.com>
2017-07-21 16:10:51 +0300

    Add JavaDoc for JavaSyntaxChecker.

--- src/main/java/pingis/utils/JavaSyntaxChecker.java
+++ src/main/java/pingis/utils/JavaSyntaxChecker.java
@@ -5,10 +5,17 @@ import com.github.javaparser.ParseProblemException;
 import com.github.javaparser.ast.CompilationUnit;
 
 /**
+ * Helper class for checking the syntax of Java code.
  * Created by dwarfcrank on 7/21/17.
  */
 public class JavaSyntaxChecker {
 
+    /**
+     * Parses the code to check its syntax.
+     * @param code Code to parse. Expects a whole Java compilation unit (i.e. package declarations/imports, class
+     *             definitions, etc.).
+     * @return Array of strings describing the encountered parse errors, or null if the syntax is correct.
+     */
     public static String[] parseCode(String code) {
         try {
             CompilationUnit cu = JavaParser.parse(code);
