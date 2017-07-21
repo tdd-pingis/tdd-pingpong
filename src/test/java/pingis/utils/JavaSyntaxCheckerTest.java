c27ffa08fa1b96d1c818e41ddbb9a30284d60b3e
Matias Juntunen <matias.juntunen@gmail.com>
2017-07-21 16:18:15 +0300

    Fix checkstyle complaints.

--- src/test/java/pingis/utils/JavaSyntaxCheckerTest.java
+++ src/test/java/pingis/utils/JavaSyntaxCheckerTest.java
@@ -20,12 +20,12 @@ public class JavaSyntaxCheckerTest {
     public void testInvalidClassDeclaration() {
         final String code = "public class Broken {";
         final String[] expected = new String[] {
-                "(line 1,col 21) Parse error. Found <EOF>, expected one of  \";\" \"<\" \"@\" \"abstract\" "
-                        + "\"boolean\" \"byte\" \"char\" \"class\" \"default\" \"double\" \"enum\" \"exports\" "
-                        + "\"final\" \"float\" \"int\" \"interface\" \"long\" \"module\" \"native\" \"open\" "
-                        + "\"opens\" \"private\" \"protected\" \"provides\" \"public\" \"requires\" \"short\" "
-                        + "\"static\" \"strictfp\" \"synchronized\" \"to\" \"transient\" \"transitive\" \"uses\" "
-                        + "\"void\" \"volatile\" \"with\" \"{\" \"}\" <IDENTIFIER>"
+            "(line 1,col 21) Parse error. Found <EOF>, expected one of  \";\" \"<\" \"@\" \"abstract\" "
+                    + "\"boolean\" \"byte\" \"char\" \"class\" \"default\" \"double\" \"enum\" \"exports\" "
+                    + "\"final\" \"float\" \"int\" \"interface\" \"long\" \"module\" \"native\" \"open\" "
+                    + "\"opens\" \"private\" \"protected\" \"provides\" \"public\" \"requires\" \"short\" "
+                    + "\"static\" \"strictfp\" \"synchronized\" \"to\" \"transient\" \"transitive\" \"uses\" "
+                    + "\"void\" \"volatile\" \"with\" \"{\" \"}\" <IDENTIFIER>"
         };
 
         String[] errors = JavaSyntaxChecker.parseCode(code);
