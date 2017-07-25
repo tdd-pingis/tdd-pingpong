77bfaa4694974b93126ea8e4c9e3ef4165b37336
Matias Juntunen <matias.juntunen@gmail.com>
2017-07-25 11:17:23 +0300

    Add an example exercise and its dependencies as the base for a project template.

---
+++ tmc-assets/submission-template/src/Koira.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author s1400591
+ */
+public class Koira {
+    
+    private String nimi;
+    private String rotu;
+    private int ika;    
+}
