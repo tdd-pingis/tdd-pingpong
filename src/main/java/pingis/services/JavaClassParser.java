7f5d96586cfff14abd9c9d4fcfa6e007d5b13ffb
Ville Saarinen <ville.saarinen@helsinki.fi>
2017-07-25 12:09:17 +0300

    Proper class parsing added

---
+++ src/main/java/pingis/services/JavaClassParser.java
@@ -0,0 +1,50 @@
+
+package pingis.services;
+
+import java.util.List;
+import pingis.entities.Challenge;
+import pingis.entities.Task;
+
+public class JavaClassParser {
+
+    public JavaClassParser() {
+    }   
+    
+    public String parseChallenge(Challenge challenge, List<Task> tasks) {
+        String classCode = "\n" + generateTestClassHeader(challenge);
+        classCode += "\n\n" + indentateTaskAsMethod(tasks);
+        classCode += generateClassEnd();
+        
+        // before return replace all \n with system-independent line-separators
+        classCode.replace("\n", System.getProperty("line.separator"));
+        
+        return classCode;
+    } 
+    
+    private String generateTestClassHeader(Challenge c) {
+        return "public class " + c.getName() + "Test {";
+    }
+    
+    private String generateImplementationClassHeader(Challenge c) {
+        return "public class " + c.getName() + " {";
+    }
+    
+    private String generateClassEnd() {
+        return "\n}";
+    }
+    
+    private String indentateTaskAsMethod(List<Task> tasks) {
+        String codeSegment = "";
+        
+        for (Task task : tasks) {
+            String[] lines = task.getCode().split(System.getProperty("line.separator"));
+            for (String line : lines) {
+                codeSegment += "\t" + line + "\n";
+            }
+            codeSegment += "\n";
+        }
+        
+        return codeSegment;
+    }
+    
+}
