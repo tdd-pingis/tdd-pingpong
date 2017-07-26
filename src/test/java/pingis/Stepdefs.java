c2a57ca4d7f3735aa745886ca47295f2fba55855
Heliozoa <dm89132@gmail.com>
2017-07-26 03:02:02 +0300

    Set up Selenium

--- src/test/java/pingis/Stepdefs.java
+++ src/test/java/pingis/Stepdefs.java
@@ -1,34 +1,75 @@
 package pingis;
 
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import static org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest()
 public class Stepdefs {
+    
+    WebDriver driver;
+    String baseUrl;
 
     @Test
     public void setupTest() {
     }
 
-    @Given("^the test is run$")
-    public void the_test_is_run() throws Throwable {
-        assertTrue(true);
+    @Before
+    public void setUp() throws Exception {
+        if(driver == null) driver = new HtmlUnitDriver();
+        baseUrl = "http://localhost:8080/";
     }
 
-    @When("^the test has finished$")
-    public void the_test_has_finished() throws Throwable {
-        assertTrue(true);
+    private void get(String page){
+        driver.get(baseUrl + page);
     }
 
-    @Then("^there are no errors$")
-    public void there_are_no_errors() throws Throwable {
-        assertTrue(true);
+    private boolean contains(String s){
+        return driver.getPageSource().contains(s);
+    }
+
+    @Given("^.* navigates to the login form$")
+    public void navigates_to_the_login_form() throws Throwable {
+        get("login");
+    }
+
+    @When("^.* inputs their username (.*) and password (.*)$")
+    public void inputs_username_and_password(String username, String password) throws Throwable {
+        driver.findElement(By.id("username")).sendKeys(username);
+        driver.findElement(By.id("password")).sendKeys(password);
+    }
+
+    @And("submits the .*")
+    public void submits() throws Throwable {
+        driver.findElement(By.id("submit")).click();
+    }
+
+    @Then("^.* is successfully authenticated$")
+    public void successfully_authenticated() throws Throwable {
+        assertFalse(contains("error"));
+    }
+
+    @Then("^.* is not authenticated$")
+    public void not_authenticated() throws Throwable {
+        assertTrue(contains("error"));
+    }
+
+    @After
+    public void tearDown() {
+        driver.close();
+        driver.quit();
     }
 }
---
+++ src/test/resources/pingis/login.feature
@@ -0,0 +1,25 @@
+Feature: Cucumber works
+
+Scenario Outline: <user> can log in with valid username and password
+    Given <user> navigates to the login form
+    When <user> inputs their username <username> and password <password>
+    And submits the form
+    Then <user> is successfully authenticated
+
+    Examples:
+        |user   |username   |password   |
+        |User   |user       |password   |
+        |Admin  |admin      |password   |
+
+Scenario Outline: <user> cannot login with an invalid username or password
+    Given <user> navigates to the login form
+    When <user> inputs their username <username> and password <password>
+    And submits the form
+    Then <user> is not authenticated
+
+    Examples:
+        |user   |username   |password   |
+        |User   |user       |passord    |
+        |Admin  |admin      |passord    |
+        |User   |uer        |password   |
+        |Admin  |amin       |password   |
