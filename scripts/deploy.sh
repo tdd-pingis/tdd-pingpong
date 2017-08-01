073c3898d3f9f9562098f3be593c58fd63af53e4
impliedfeline <kinnunen.jussi@hotmail.com>
2017-08-01 18:10:17 +0300

    Fix whitespace

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,6 @@ if [[ "${TRAVIS_BRANCH}" = "master" && "${TRAVIS_PULL_REQUEST}" = "false" ]]; th
 	docker login -u="${DOCKER_USER}" -p="${DOCKER_PASS}"
 	docker build -t ${DOCKER_USER}/${DOCKER_REPO} .
 	docker push ${DOCKER_USER}/${DOCKER_REPO}
-    sshpass -e ssh -p ${DEPLOY_PORT} ${DEPLOY_USER}@${DEPLOY_HOST} 'deploy.sh'
+	sshpass -e ssh -p ${DEPLOY_PORT} ${DEPLOY_USER}@${DEPLOY_HOST} 'deploy.sh'
 fi
 
