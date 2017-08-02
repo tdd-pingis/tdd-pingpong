cb6340276a54e88291ee7a1f33cc2738dc984b68
Lauri Pulkkinen <lauri.pulkkinen@helsinki.fi>
2017-08-02 15:19:49 +0300

    Created TaskService

---
+++ src/main/java/pingis/services/TaskService.java
@@ -0,0 +1,47 @@
+package pingis.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pingis.entities.Task;
+import pingis.repositories.ChallengeRepository;
+import pingis.repositories.TaskRepository;
+import java.util.List;
+
+@Service
+public class TaskService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+    @Autowired
+    private ChallengeRepository challengeRepository;
+
+    public Task findTaskInChallenge(Long challengeId, int taskId) {
+        // Implement validation here
+        return challengeRepository.findOne(challengeId).getTasks().get(taskId);
+    }
+
+    public Task findOne(Long taskId) {
+        // Implement validation here
+        return taskRepository.findOne(taskId);
+    }
+
+    public Task save(Task newTask) {
+        // Implement validation here
+        return taskRepository.save(newTask);
+    }
+
+    public List<Task> findAll() {
+        return (List) taskRepository.findAll();
+    }
+
+    public Task delete(Long taskId) {
+        //Implement validation here
+        Task t = findOne(taskId);
+        taskRepository.delete(taskId);
+        return t;
+    }
+
+    public boolean contains(Long taskId) {
+        return taskRepository.exists(taskId);
+    }
+}
