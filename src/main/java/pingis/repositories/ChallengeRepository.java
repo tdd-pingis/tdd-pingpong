65c8e3dcfce68da7e49ed44b34e45cfb4b22c182
laurpulk <lauri.pulkkinen@helsinki.fi>
2017-08-03 11:10:23 +0300

    Refactoring controllers (#69)
    * Created MiscController
    
    * Created ChallengeService
    
    * Created TaskService
    
    * Created EditorService
    
    * Renamed ChallengeController to TaskController & Refactored necessary files
    
    * Fixed testdata parameters
    
    * Removed sandbox template test
    
    * Removed sandbox mapping
    
    * Removed sandbox.html
    
    * Renamed MiscController to IndexController

--- src/main/java/pingis/repositories/ChallengeRepository.java
+++ src/main/java/pingis/repositories/ChallengeRepository.java
@@ -5,4 +5,5 @@ import pingis.entities.Challenge;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ChallengeRepository extends CrudRepository<Challenge, Long> {
+    Challenge findByName(String name);
 }
