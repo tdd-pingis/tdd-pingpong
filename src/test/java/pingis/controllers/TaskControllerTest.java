64706f3aab5e08e6a9deb86e68939d672af07e29
Matias Juntunen <matias.juntunen@gmail.com>
2017-08-04 13:45:23 +0300

    Add a test case for TaskController.

--- src/test/java/pingis/controllers/TaskControllerTest.java
+++ src/test/java/pingis/controllers/TaskControllerTest.java
@@ -3,6 +3,9 @@ package pingis.controllers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -13,12 +16,20 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import pingis.config.SecurityDevConfig;
-import pingis.entities.*;
+import pingis.entities.Challenge;
+import pingis.entities.ImplementationType;
+import pingis.entities.Task;
+import pingis.entities.User;
 import pingis.services.*;
+import pingis.utils.JavaClassGenerator;
+
+import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.assertArrayEquals;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
@@ -48,11 +59,48 @@ public class TaskControllerTest {
     @MockBean
     private EditorService editorServiceMock;
 
+    @Captor
+    private ArgumentCaptor<Map<String, byte[]>> packagingArgCaptor;
+
     @Before
     public void setUp() {
+        MockitoAnnotations.initMocks(this);
+
         this.mvc = MockMvcBuilders
                 .webAppContextSetup(context)
                 .build();
+    }
+
+    @Test
+    public void submitTask() throws Exception {
+        User testUser = new User(1, "TESTUSER", 1);
+        Challenge challenge = new Challenge("Calculator", testUser, "Simple calculator");
+        Task task = new Task(1, ImplementationType.TEST, testUser,
+                "CalculatorAddition","Implement addition", "return 1+1;", 1, 1);
+
+        String implFileName = JavaClassGenerator.generateImplClassFilename(challenge);
+        String implCode = "/* this is an implementation */";
+        String testFileName = JavaClassGenerator.generateTestClassFilename(challenge);
+        String testCode = "/* this is a test */";
+
+        when(challengeServiceMock.findOne(challenge.getId())).thenReturn(challenge);
+        when(taskServiceMock.findTaskInChallenge(challenge.getId(), task.getTaskId())).thenReturn(task);
+
+        mvc.perform(post("/task")
+                .param("implementationCode", implCode)
+                .param("testCode", testCode)
+                .param("challengeId", Long.toString(challenge.getId()))
+                .param("taskId", Long.toString(task.getTaskId())))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrlPattern("/feedback*"));
+
+        verify(packagingService).packageSubmission(packagingArgCaptor.capture());
+
+        Map<String, byte[]> files = packagingArgCaptor.getValue();
+        assertArrayEquals(implCode.getBytes(), files.get(implFileName));
+        assertArrayEquals(testCode.getBytes(), files.get(testFileName));
+
+        verifyNoMoreInteractions(packagingService);
     }
 
     @Test
