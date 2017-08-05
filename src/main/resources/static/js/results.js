91cd02f4bdd4d94fcec3bd1c0b9d0d4b1e4feb71
Heliozoa <dm89132@gmail.com>
2017-08-05 18:41:48 +0300

    Results are now printed in a legible format, though still just plaintext

--- src/main/resources/static/js/results.js
+++ src/main/resources/static/js/results.js
@@ -8,5 +8,29 @@ function connect() {
 }
 
 function showResults(results) {
-    $("#results").html(results.body);
+    var testOutput = JSON.parse(JSON.parse(results.body).testOutput)
+    var status = testOutput.status;
+    var results = testOutput.testResults;
+    
+    $("#results").html("<div>"+ status +"</div>");
+    
+    for(i = 0; i < results.length; i++) {
+        var res = results[i]
+        var name = res.name;
+        var passed = res.passed;
+        var points = res.points;
+        var errorMessage = res.errorMessage;
+        var backtrace = res.backtrace;
+        
+        $("#results").append("<div>Test name: "+ name +"</div>")
+        $("#results").append("<div>Passed: "+ passed +"</div>")
+        $("#results").append("<div>Points: "+ points +"</div>")
+        $("#results").append("<div>Error message: "+ errorMessage +"</div>")
+        
+        var traceId = "backtrace"+i
+        $("#results").append("<div id="+traceId+">Backtrace: </div>")
+        for(j = 0; j < backtrace.length; j++) {
+            $("#"+traceId).append("<div> "+ backtrace[j] +"</div>")
+        }
+    }
 }
