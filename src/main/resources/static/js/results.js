48e66ce9ef5572e662981065d95710f8a3e7ab09
Heliozoa <dm89132@gmail.com>
2017-08-07 02:44:33 +0300

    Improved look and security of the result printing

--- src/main/resources/static/js/results.js
+++ src/main/resources/static/js/results.js
@@ -12,40 +12,99 @@ function connect() {
 function connectStomp(csrf) {
     var headerName = csrf.headerName;
     var token = csrf.token;
-    
+
     var headers = {};
     headers[headerName] = token;
-    
+
     stompClient = Stomp.over(new SockJS('/websocket'));
     stompClient.connect(headers, function () {
         stompClient.subscribe('/topic/results', showResults);
     });
 }
 
-function showResults(results) {
-    var testOutput = JSON.parse(JSON.parse(results.body).testOutput)
+function showResults(response) {
+    var responseBody = JSON.parse(response.body);
+    var testOutput = JSON.parse(responseBody.testOutput);
+
     var status = testOutput.status;
-    var results = testOutput.testResults;
-    
-    $("#results").html("<div>"+ status +"</div>");
-    
-    for(i = 0; i < results.length; i++) {
-        var res = results[i]
+    var testResults = testOutput.testResults;
+
+    var results = $("#results");
+    results.html(
+            $("<div>")
+            .addClass("col-md-6"));
+    results.append($("<div>")
+            .attr("id", "panel")
+            .addClass("panel panel-default"));
+
+    var panel = $("#panel")
+    panel.append(
+            resultDiv(status));
+    panel.append(
+            $("<div>")
+            .attr("id", "panel-body")
+            .addClass("panel-body"));
+
+    var panelBody = $("#panel-body")
+
+    for (i = 0; i < testResults.length; i++) {
+        
+        var res = testResults[i];
         var name = res.name;
         var passed = res.passed;
         var points = res.points;
         var errorMessage = res.errorMessage;
         var backtrace = res.backtrace;
-        
-        $("#results").append("<div>Test name: "+ name +"</div>")
-        $("#results").append("<div>Passed: "+ passed +"</div>")
-        $("#results").append("<div>Points: "+ points +"</div>")
-        $("#results").append("<div>Error message: "+ errorMessage +"</div>")
-        
-        var traceId = "backtrace"+i
-        $("#results").append("<div id="+traceId+">Backtrace: </div>")
-        for(j = 0; j < backtrace.length; j++) {
-            $("#"+traceId).append("<div> "+ backtrace[j] +"</div>")
+        var testId = "test" + i;
+        var traceId = "backtrace" + i;
+
+        panelBody.append(
+                $("<div>")
+                .attr("id", testId))
+
+        var test = $("#"+testId);
+        test.append($("<div>")
+                .text("Test name: " + name));
+        test.append($("<div>")
+                .text("Passed: " + passed));
+        test.append($("<div>")
+                .text("Points: " + points));
+        test.append($("<div>")
+                .text("Error message :" + errorMessage));
+
+        test.append(
+                $("<div>")
+                .attr("id", traceId)
+                .text("Backtrace: "));
+
+        var trace = $("#" + traceId);
+        for (j = 0; j < backtrace.length; j++) {
+            trace.append(
+                    $("<div>")
+                    .text(backtrace[j]));
         }
+        
+        test.append(
+                $("<br>"));
+    }
+}
+
+function resultDiv(status) {
+    var resultDiv = $("<div>");
+    
+    if (status === 'TESTS_PASSED') {
+        return resultDiv
+                .addClass("alert alert-success")
+                .text("Success!!");
+        
+    } else if (status === 'TESTS_FAILED') {
+        return resultDiv
+                .addClass("alert alert-danger")
+                .text("Some tests failed");
+        
+    } else {
+        return resultDiv
+                .addClass("alert alert-warning")
+                .text("Something happened: "+ status);
     }
 }
