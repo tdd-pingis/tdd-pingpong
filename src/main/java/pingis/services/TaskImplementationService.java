67d8c17af223152ea0c5dac54146f75591a3d16e
Lauri Pulkkinen <lauri.pulkkinen@helsinki.fi>
2017-08-08 18:09:14 +0300

    Add updateTaskImplementationCode() method to TaskImplService

--- src/main/java/pingis/services/TaskImplementationService.java
+++ src/main/java/pingis/services/TaskImplementationService.java
@@ -3,6 +3,7 @@ package pingis.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import pingis.entities.TaskImplementation;
 import pingis.repositories.TaskImplementationRepository;
 import pingis.repositories.TaskRepository;
@@ -43,5 +44,12 @@ public class TaskImplementationService {
     public TaskImplementation findOne(long taskImplementationId) {
         return taskImplementationRepository.findOne(taskImplementationId);
 
+    }
+
+    @Transactional
+    public TaskImplementation updateTaskImplementationCode(Long taskImplementationId, String taskImplementationCode) {
+        TaskImplementation taskImplementationToUpdate = taskImplementationRepository.findOne(taskImplementationId);
+        taskImplementationToUpdate.setCode(taskImplementationCode);
+        return taskImplementationToUpdate;
     }
 }
