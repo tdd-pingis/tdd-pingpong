4d97d29cfc6b9faff904035a160d515b79992a92
Matias Juntunen <matias.juntunen@gmail.com>
2017-08-16 10:54:19 +0300

    Make controllers use SandboxService.

--- src/main/java/pingis/controllers/TmcSubmissionController.java
+++ src/main/java/pingis/controllers/TmcSubmissionController.java
@@ -1,27 +1,16 @@
 package pingis.controllers;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.util.UUID;
-import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import pingis.entities.TaskType;
-import pingis.entities.sandbox.Logs;
-import pingis.entities.sandbox.ResultMessage;
-import pingis.entities.sandbox.ResultStatus;
-import pingis.entities.sandbox.Submission;
-import pingis.entities.sandbox.SubmissionStatus;
-import pingis.entities.sandbox.TestOutput;
-import pingis.repositories.sandbox.SubmissionRepository;
-import pingis.services.TaskInstanceService;
+import pingis.services.sandbox.SandboxService;
 
 @Controller
 public class TmcSubmissionController {
@@ -29,11 +18,7 @@ public class TmcSubmissionController {
   private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
   @Autowired
-  private SubmissionRepository submissionRepository;
-  @Autowired
-  private SimpMessagingTemplate template;
-  @Autowired
-  private TaskInstanceService taskInstanceService;
+  private SandboxService sandboxService;
 
   // These request parameters are specified separately because there doesn't seem to
   // be a simple way to rename fields when doing data binding.
@@ -48,66 +33,17 @@ public class TmcSubmissionController {
       @RequestParam String status,
       @RequestParam("exit_code") String exitCode) throws IOException {
     logger.debug("Received a response from TMC sandbox");
+
     UUID submissionId = UUID.fromString(token);
-    Submission submission = submissionRepository.findOne(submissionId);
-
-    if (submission == null) {
-      return new ResponseEntity(HttpStatus.NOT_FOUND);
-    } else if (submission.getStatus() != SubmissionStatus.PENDING) {
-      // Result is being submitted twice.
-      // TODO: decide on a better response code for this...
-      return new ResponseEntity(HttpStatus.BAD_REQUEST);
-    }
-
     int exitCodeValue = Integer.parseInt(exitCode.trim());
-    ObjectMapper mapper = new ObjectMapper();
-    submission.setExitCode(exitCodeValue);
-    submission.setStatus(status);
-    submission.setStderr(stderr);
-    submission.setStdout(stdout);
-    submission.setTestOutput(mapper.readValue(testOutput, TestOutput.class));
-    submission.setValidations(validations);
-    submission.setVmLog(vmLog);
-    submissionRepository.save(submission);
-    sendResults(submission);
+
+    boolean updated = sandboxService.updateSubmissionResult(submissionId, testOutput, stdout,
+        stderr, validations, vmLog, status, exitCodeValue);
+
+    if (!updated) {
+      return new ResponseEntity(HttpStatus.NOT_FOUND);
+    }
+
     return new ResponseEntity(HttpStatus.OK);
-  }
-
-  private void sendResults(Submission submission) {
-    ResultMessage message = createMessage(submission);
-    if (message.isSuccess()) {
-      taskInstanceService.markAsDone(submission.getTaskInstance());
-    }
-    //Broadcasts the submission to /topic/results
-    this.template.convertAndSend("/topic/results/" + submission.getId(), message);
-    logger.debug("Sent the TMC sandbox results to /topic/results");
-  }
-
-  private ResultMessage createMessage(Submission submission) {
-    TestOutput top = submission.getTestOutput();
-    Logs logs = top.getLogs();
-    ResultStatus status = top.getStatus();
-
-    ResultMessage message = new ResultMessage();
-    TaskType type = submission.getTaskInstance().getTask().getType();
-    message.setType(type);
-    message.setStatus(status);
-
-    if (status == ResultStatus.COMPILE_FAILED) {
-      message.setStdout(logs.getStdoutString());
-    } else if (status == ResultStatus.PASSED) {
-      message.setSuccess(type == TaskType.IMPLEMENTATION);
-      message.setTests(
-          top.getTestResults().stream()
-              .filter(r -> r.isPassed()).collect(Collectors.toList()));
-
-    } else if (status == ResultStatus.TESTS_FAILED) {
-      message.setSuccess(type == TaskType.TEST);
-      message.setTests(
-          top.getTestResults().stream()
-              .filter(r -> !r.isPassed()).collect(Collectors.toList()));
-    }
-
-    return message;
   }
 }
