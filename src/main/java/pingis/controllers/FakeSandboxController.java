2f7a3bfd43d670d9b48734f6459e977259440aef
impliedfeline <kinnunen.jussi@hotmail.com>
2017-08-17 16:08:48 +0300

    Rename class

--- src/main/java/pingis/controllers/FakeSandboxController.java
+++ src/main/java/pingis/controllers/FakeSandboxController.java
@@ -5,10 +5,13 @@
  */
 package pingis.controllers;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.util.UUID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -20,6 +23,10 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
+import pingis.entities.sandbox.Submission;
+import pingis.entities.sandbox.SubmissionStatus;
+import pingis.entities.sandbox.TestOutput;
+import pingis.repositories.sandbox.SubmissionRepository;
 import pingis.services.sandbox.SubmissionResponse;
 
 /**
@@ -27,68 +34,80 @@ import pingis.services.sandbox.SubmissionResponse;
  * @author authority
  */
 @Controller
-public class FakeSanboxController {
+public class FakeSandboxController {
 
   Logger logger = LoggerFactory.getLogger(this.getClass());
+  
+  @Autowired
+  SubmissionRepository submissionRepository;
 
   @RequestMapping("/tasks.json")
   public ResponseEntity tasks(
       @RequestParam("token") String token) {
 
-    logger.debug("help help!!!!!");
     logger.debug("TOKEN::::::"+token);
 
     RestTemplate restTemplate = new RestTemplate();
-    Map<String, String> params = new HashMap<String, String>();
 
     HttpHeaders headers = new HttpHeaders();
     headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-    MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
-    map.add("test_output", "{"+
-            "\"status\":\"PASSED\","+
-            "\"testResults\":"+
-            "    ["+
-            "        {"+
-            "            \"name\":\"CalculatorTest succTest\","+
-            "            \"passed\":true,"+
-            "            \"points\":[\"03-03\"],"+
-            "            \"errorMessage\":\"\","+
-            "            \"backtrace\":[]"+
-            "        },"+
-            "        {"+
-            "            \"name\":\"CalculatorTest failTest\","+
-            "            \"passed\":true,"+
-            "            \"points\":[\"03-03\"],"+
-            "            \"errorMessage\":\"\","+
-            "            \"backtrace\":[]"+
-            "        },"+
-            "        {"+
-            "            \"name\":\"CalculatorTest equalTest\","+
-            "            \"passed\":true,"+
-            "            \"points\":[\"03-03\"],"+
-            "            \"errorMessage\":\"\","+
-            "            \"backtrace\":[]"+
-            "        }"+
-            "    ],"+
-            "    \"logs\":"+
-            "        {"+
-            "            \"stdout\":[],"+
-            "            \"stderr\":[]"+
-            "        }"+
-        "}");
-    map.add("stdout", "");
-    map.add("stderr", "");
-    map.add("vm_log", "");
-    map.add("validations", "{}");
-    map.add("token", token);
-    map.add("exit_code", "0");
-    map.add("status", "FINISHED");
+    HttpEntity<MultiValueMap<String, String>> request = buildResponseEntity(
+          loadSubmission(token), headers);
     
-    HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(map, headers);
     restTemplate.postForLocation("http://localhost:8080/submission-result", request, String.class);
 
     SubmissionResponse sr = new SubmissionResponse();
     sr.setStatus("ok");
     return ResponseEntity.status(HttpStatus.OK).body(sr);
+  }
+  
+  //TODO: Actually load, don't generate
+  private Submission loadSubmission(String token) {
+    Submission submission = submissionRepository.findOne(UUID.fromString(token));
+    submission.setId(UUID.fromString(token));
+    submission.setStatus(SubmissionStatus.FINISHED);
+    submission.setExitCode(0);
+    submission.setStderr("");
+    submission.setStdout("");
+    try {
+      submission.setTestOutput(new ObjectMapper().readValue(
+            "{\"status\":\"PASSED\","
+            + "\"testResults\":[],"
+            + "\"logs\":{"
+            + "\"stdout\":[0],"
+            + "\"stderr\":[0]"
+            + "}"
+            + "}", TestOutput.class));
+    } catch (IOException ex) {
+      logger.debug("JSON serialization failed");
+    }
+    submission.setValidations("");
+    submission.setVmLog("");
+
+    //submissionRepository.save(submission);
+
+    return submission;
+  }
+  
+  private HttpEntity<MultiValueMap<String, String>> buildResponseEntity(
+        Submission submission, HttpHeaders headers) {
+    MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+    
+    try {
+      map.add("test_output", new ObjectMapper().writeValueAsString(
+            submission.getTestOutput()));
+    } catch (JsonProcessingException ex) {
+      logger.debug("POJO deserialization failed");
+    }
+
+    map.add("stdout", submission.getStdout());
+    map.add("stderr", submission.getStderr());
+    map.add("validations", submission.getValidations());
+    map.add("vm_log", submission.getVmLog());
+    map.add("token", submission.getId().toString());
+    map.add("status", submission.getStatus().toString());
+    map.add("exit_code", submission.getExitCode().toString());
+    
+    return new HttpEntity<>(map, headers);
   }
 }
