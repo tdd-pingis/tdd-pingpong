22a17b0ef66423d72aa20a9116b398047bafeb6f
Matias Juntunen <matias.juntunen@gmail.com>
2017-08-18 14:17:38 +0300

    Fix Cucumber tests.

--- src/main/java/pingis/config/WebSocketSecurityConfig.java
+++ src/main/java/pingis/config/WebSocketSecurityConfig.java
@@ -2,18 +2,24 @@ package pingis.config;
 
 import org.eclipse.jetty.websocket.api.WebSocketBehavior;
 import org.eclipse.jetty.websocket.api.WebSocketPolicy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.StompWebSocketEndpointRegistration;
 import org.springframework.web.socket.server.jetty.JettyRequestUpgradeStrategy;
 import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketSecurityConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer {
+
+  @Autowired
+  private ApplicationContext applicationContext;
 
   @Override
   public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -31,8 +37,15 @@ public class WebSocketSecurityConfig extends AbstractSecurityWebSocketMessageBro
 
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
-    registry.addEndpoint("/websocket")
-        .setHandshakeHandler(handshakeHandler())
-        .withSockJS();
+    StompWebSocketEndpointRegistration endpointRegistration = registry
+        .addEndpoint("/websocket");
+
+    // HACK: Can't use JettyRequestUpgradeStrategy when running cucumber tests, because
+    // Jetty isn't running.
+    if (applicationContext.getEnvironment().getProperty("RUNNING_CUCUMBER") == null) {
+      endpointRegistration.setHandshakeHandler(handshakeHandler());
+    }
+
+    endpointRegistration.withSockJS();
   }
 }
