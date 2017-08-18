23b6a021a882bfa23b592838fa359c88fc2c316e
Matias Juntunen <matias.juntunen@gmail.com>
2017-08-18 13:26:33 +0300

    Use a custom WebSocket handshake handler.

--- src/main/java/pingis/config/WebSocketSecurityConfig.java
+++ src/main/java/pingis/config/WebSocketSecurityConfig.java
@@ -1,10 +1,15 @@
 package pingis.config;
 
+import org.eclipse.jetty.websocket.api.WebSocketBehavior;
+import org.eclipse.jetty.websocket.api.WebSocketPolicy;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.server.jetty.JettyRequestUpgradeStrategy;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -15,8 +20,19 @@ public class WebSocketSecurityConfig extends AbstractSecurityWebSocketMessageBro
     config.enableSimpleBroker("/topic");
   }
 
+  @Bean
+  public DefaultHandshakeHandler handshakeHandler() {
+    WebSocketPolicy policy = new WebSocketPolicy(WebSocketBehavior.SERVER);
+    policy.setInputBufferSize(8192);
+    policy.setIdleTimeout(600000);
+
+    return new DefaultHandshakeHandler(new JettyRequestUpgradeStrategy(policy));
+  }
+
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
-    registry.addEndpoint("/websocket").withSockJS();
+    registry.addEndpoint("/websocket")
+        .setHandshakeHandler(handshakeHandler())
+        .withSockJS();
   }
 }
