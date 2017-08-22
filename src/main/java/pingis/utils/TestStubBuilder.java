d80746906fc112ea744cfc65b4ee50a12ba58097
Matias Juntunen <matias.juntunen@gmail.com>
2017-08-22 15:01:23 +0300

    Add TestStubBuilder.

---
+++ src/main/java/pingis/utils/TestStubBuilder.java
@@ -0,0 +1,70 @@
+package pingis.utils;
+
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.Modifier;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
+
+public class TestStubBuilder extends CodeStubBuilder {
+
+  private CompilationUnit targetCompilationUnit;
+
+  public TestStubBuilder(String code) {
+    // Need to supply a fake class name here, because we don't yet know the actual
+    // name of the class.
+    super("__PLACEHOLDER__");
+
+    targetCompilationUnit = JavaParser.parse(code);
+
+    generateTestClass();
+  }
+
+  private ClassOrInterfaceDeclaration getFirstDeclaredClass() {
+    return targetCompilationUnit.getTypes().stream()
+        .filter(typeDecl -> typeDecl instanceof ClassOrInterfaceDeclaration)
+        .map(typeDecl -> (ClassOrInterfaceDeclaration) typeDecl)
+        .findFirst().get();
+  }
+
+  private String generateTestMethodName(String methodName) {
+    String remainder = methodName.length() > 1 ? methodName.substring(1) : "";
+    return String.format("test%c%s", Character.toUpperCase(methodName.charAt(0)), remainder);
+  }
+
+  private String generateTestClassName(ClassOrInterfaceDeclaration target) {
+    return String.format("%sTest", target.getNameAsString());
+  }
+
+  private void addTestMethod(ClassOrInterfaceDeclaration clazz, MethodDeclaration method) {
+    String testMethodName = generateTestMethodName(method.getNameAsString());
+    MethodDeclaration testMethod = clazz.addMethod(testMethodName, Modifier.PUBLIC);
+
+    testMethod.addMarkerAnnotation("Test");
+  }
+
+  private void generateTestClass() {
+    ClassOrInterfaceDeclaration firstClass = getFirstDeclaredClass();
+
+    clazz.setName(generateTestClassName(firstClass));
+
+    // TODO: What value should @Points have?
+    clazz.addSingleMemberAnnotation("Points", "\"03-03\"");
+
+    firstClass.getMethods().stream()
+        .filter(method -> method.isPublic())
+        .forEach(method -> addTestMethod(clazz, method));
+  }
+
+  public CodeStubBuilder withTestImports() {
+    compilationUnit.addImport("org.junit.Test");
+    compilationUnit.addImport("fi.helsinki.cs.tmc.edutestutils.Points");
+    compilationUnit.addImport("org.junit.Assert", true, true);
+
+    return this;
+  }
+
+  public String build() {
+    return compilationUnit.toString();
+  }
+}
---
+++ src/test/java/pingis/utils/TestStubBuilderTest.java
@@ -0,0 +1,40 @@
+package pingis.utils;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TestStubBuilderTest {
+
+  private static final String SINGLE_METHOD_CLASS =
+      "public class TestMe {"
+          + "    public int something(int a, int b) {"
+          + "        return a + b;"
+          + "    }"
+          + "}";
+
+  private static final String SINGLE_METHOD_CLASS_EXPECTED =
+      "import org.junit.Test;\n"
+          + "import fi.helsinki.cs.tmc.edutestutils.Points;\n"
+          + "import static org.junit.Assert.*;\n"
+          + "\n"
+          + "@Points(\"03-03\")\n"
+          + "public class TestMeTest {\n"
+          + "\n"
+          + "    @Test\n"
+          + "    public void testSomething() {\n"
+          + "    }\n"
+          + "}\n";
+
+  // TODO: More test cases
+
+  @Test
+  public void testMethod() {
+    String code = new TestStubBuilder(SINGLE_METHOD_CLASS)
+        .withTestImports()
+        .build();
+
+    assertEquals(SINGLE_METHOD_CLASS_EXPECTED, code);
+  }
+
+}
