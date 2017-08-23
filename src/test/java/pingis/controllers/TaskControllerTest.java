0117ee3fe306ebbb8fc0db9f8327543e3c838e16
Heliozoa <dm89132@gmail.com>
2017-08-23 13:55:20 +0300

    More tests for task controller

--- src/test/java/pingis/controllers/TaskControllerTest.java
+++ src/test/java/pingis/controllers/TaskControllerTest.java
@@ -1,6 +1,10 @@
 package pingis.controllers;
 
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
@@ -8,12 +12,17 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrlPattern;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import java.util.Iterator;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.UUID;
 import org.junit.Before;
 import org.junit.Test;
@@ -30,6 +39,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.context.WebApplicationContext;
 import pingis.config.OAuthProperties;
 import pingis.config.SecurityConfig;
@@ -229,5 +239,176 @@ public class TaskControllerTest {
         .andExpect(status().isOk())
         .andExpect(view().name(viewName))
         .andExpect(content().string(containsString(expectedContent)));
+  }
+
+  @Test
+  public void nextTaskReturnsNextTaskView() throws Exception {
+    Long challengeId = 678L;
+
+    Challenge challenge = mock(Challenge.class);
+
+    Iterator itr = mock(Iterator.class);
+    when(itr.hasNext()).thenReturn(false);
+
+    List testTasks = mock(List.class);
+    when(testTasks.iterator()).thenReturn(itr);
+
+    Set set = mock(Set.class);
+    when(set.iterator()).thenReturn(itr);
+
+    MultiValueMap implementationTaskInstances = mock(MultiValueMap.class);
+    when(implementationTaskInstances.keySet()).thenReturn(set);
+
+    when(challengeServiceMock.findOne(any())).thenReturn(mock(Challenge.class));
+    when(taskServiceMock.filterTasksByUser(any(), any())).thenReturn(testTasks);
+    when(taskServiceMock.getAvailableTestTaskInstances(any(), any()))
+            .thenReturn(implementationTaskInstances);
+
+    mvc.perform(get("/nextTask/" + challengeId))
+            .andExpect(status().isOk())
+            .andExpect(view().name("nexttask"))
+            .andExpect(model().attributeExists("challenge"))
+            .andExpect(model().attributeExists("testTasks"))
+            .andExpect(model().attributeExists("implementationTaskInstances"));
+
+    verify(challengeServiceMock).findOne(678L);
+  }
+
+  @Test
+  public void newImplementationTaskInstanceRedirectsToTask() throws Exception {
+    Long taskId = 123L;
+    Long testTaskInstanceId = 555L;
+    Long newTaskInstanceId = 743L;
+
+    Task task = mock(Task.class);
+    when(task.getType()).thenReturn(TaskType.IMPLEMENTATION);
+    when(taskServiceMock.findOne(any())).thenReturn(task);
+
+    TaskInstance newTaskInstance = mock(TaskInstance.class);
+    when(newTaskInstance.getId()).thenReturn(newTaskInstanceId);
+    when(taskInstanceServiceMock.createEmpty(any(), any())).thenReturn(newTaskInstance);
+
+    TaskInstance testTaskInstance = mock(TaskInstance.class);
+    when(taskInstanceServiceMock.findOne(anyLong())).thenReturn(testTaskInstance);
+
+    mvc.perform(get("/newTaskInstance")
+            .param("taskId", taskId.toString())
+            .param("testTaskInstanceId", testTaskInstanceId.toString()))
+            .andExpect(status().is3xxRedirection())
+            .andExpect(redirectedUrl("/task/" + newTaskInstanceId))
+            .andExpect(model().attribute("taskInstanceId", newTaskInstanceId.toString()));
+
+    verify(taskServiceMock).findOne(taskId);
+    verify(taskInstanceServiceMock).findOne(testTaskInstanceId);
+    verify(taskInstanceServiceMock).save(newTaskInstance);
+    verify(taskInstanceServiceMock).save(testTaskInstance);
+  }
+
+  @Test
+  public void newTestTaskInstanceRedirectsToTask() throws Exception {
+    Long taskId = 123L;
+    Long testTaskInstanceId = 234L;
+    Long newTaskInstanceId = 345L;
+
+    Task task = mock(Task.class);
+    when(task.getType()).thenReturn(TaskType.TEST);
+    when(taskServiceMock.findOne(any())).thenReturn(task);
+
+    TaskInstance newTaskInstance = mock(TaskInstance.class);
+    when(newTaskInstance.getId()).thenReturn(newTaskInstanceId);
+    when(taskInstanceServiceMock.createEmpty(any(), any())).thenReturn(newTaskInstance);
+
+    mvc.perform(get("/newTaskInstance")
+            .param("taskId", taskId.toString())
+            .param("testTaskInstanceId", testTaskInstanceId.toString()))
+            .andExpect(status().is3xxRedirection())
+            .andExpect(redirectedUrl("/task/" + newTaskInstanceId))
+            .andExpect(model().attribute("taskInstanceId", newTaskInstanceId.toString()));
+
+    verify(taskServiceMock).findOne(taskId);
+    verify(taskInstanceServiceMock, never()).save(any());
+  }
+
+  @Test
+  public void randomTaskRedirectsToRandomTask() throws Exception {
+    Long challengeId = 333L;
+
+    Challenge challenge = mock(Challenge.class);
+    when(challenge.getId()).thenReturn(challengeId);
+    when(challengeServiceMock.getRandomChallenge()).thenReturn(challenge);
+
+    mvc.perform(get("/randomTask"))
+            .andExpect(status().is3xxRedirection())
+            .andExpect(model().attribute("challengeId", challengeId.toString()))
+            .andExpect(redirectedUrlPattern("/randomTask/?*"));
+  }
+
+  @Test
+  public void randomTaskWithoutAvailableTaskRedirectsToUser() throws Exception {
+    when(taskServiceMock.noNextTaskAvailable(any(), any())).thenReturn(true);
+
+    mvc.perform(get("/randomTask/0"))
+            .andExpect(status().is3xxRedirection())
+            .andExpect(redirectedUrl("/user"));
+  }
+
+  @Test
+  public void randomTaskWithTestTaskRedirectsToNewTaskInstance() throws Exception {
+    Long challengeId = 999L;
+    Long taskId = 237L;
+
+    Task task = mock(Task.class);
+    when(task.getId()).thenReturn(taskId);
+
+    when(challengeServiceMock.findOne(any())).thenReturn(mock(Challenge.class));
+    when(userServiceMock.getCurrentUser()).thenReturn(mock(User.class));
+    when(taskServiceMock.noNextTaskAvailable(any(), any())).thenReturn(false);
+
+    when(taskServiceMock.getRandomTaskType()).thenReturn(TaskType.TEST);
+    when(taskServiceMock.hasNextTestTaskAvailable(any(), any())).thenReturn(true);
+
+    when(taskServiceMock.getRandomTestTask(any(), any())).thenReturn(task);
+
+    mvc.perform(get("/randomTask/0")
+            .param("challengeId", challengeId.toString()))
+            .andExpect(status().is3xxRedirection())
+            .andExpect(redirectedUrlPattern("/newTaskInstance?*"))
+            .andExpect(model().attribute("taskId", taskId.toString()))
+            .andExpect(model().attribute("testTaskInstanceId", "0"));
+
+    verify(taskServiceMock).getRandomTestTask(any(), any());
+  }
+
+  @Test
+  public void randomTaskWithImplementationTaskRedirectsToNewTaskInstance() throws Exception {
+    Long challengeId = 998L;
+    Long taskId = 337L;
+    Long taskInstanceId = 222L;
+
+    Task task = mock(Task.class);
+    when(task.getId()).thenReturn(taskId);
+
+    TaskInstance taskInstance = mock(TaskInstance.class);
+    when(taskInstance.getId()).thenReturn(taskInstanceId);
+
+    when(challengeServiceMock.findOne(any())).thenReturn(mock(Challenge.class));
+    when(userServiceMock.getCurrentUser()).thenReturn(mock(User.class));
+    when(taskServiceMock.noNextTaskAvailable(any(), any())).thenReturn(false);
+    when(taskServiceMock.hasNextImplTaskAvailable(any(), any())).thenReturn(true);
+
+    when(taskServiceMock.getRandomTaskType()).thenReturn(TaskType.IMPLEMENTATION);
+
+    when(taskServiceMock.getRandomImplTask(any(), any())).thenReturn(task);
+    when(taskServiceMock.getRandomTaskInstance(any(), any(), any())).thenReturn(taskInstance);
+
+    mvc.perform(get("/randomTask/0")
+            .param("challengeId", challengeId.toString()))
+            .andExpect(status().is3xxRedirection())
+            .andExpect(redirectedUrlPattern("/newTaskInstance?*"))
+            .andExpect(model().attribute("taskId", taskId.toString()))
+            .andExpect(model().attribute("testTaskInstanceId", taskInstanceId.toString()));
+
+    verify(taskServiceMock).getRandomImplTask(any(), any());
+
   }
 }
