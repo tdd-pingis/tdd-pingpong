155ab1cc5a16ee79cf4ffc88894108c0f28d3c50
lauri <lauri334@hotmail.com>
2017-08-26 13:55:02 +0300

    checkstyle

--- src/main/java/pingis/controllers/LiveChallengeController.java
+++ src/main/java/pingis/controllers/LiveChallengeController.java
@@ -307,8 +307,8 @@ public class LiveChallengeController {
     User player = userService.getCurrentUser();
     logger.info("User: " + player.getName());
 
-    if (challenge.getAuthor().equals(player) ||
-        (challenge.getSecondPlayer() != null
+    if (challenge.getAuthor().equals(player)
+        || (challenge.getSecondPlayer() != null
             && challenge.getSecondPlayer().equals(player))) {
       redirectAttributes.addFlashAttribute("message", "Cannot re-do your own live challenge");
       return new RedirectView("/error");
