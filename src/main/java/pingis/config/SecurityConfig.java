939fcff1814f7f6087146064eb6ce0496e970806
Heliozoa <dm89132@gmail.com>
2017-08-28 13:42:13 +0300

    Fixed the user page, made the user page tabs functional, added css to the dropdown and added /img to permit all in the security config

--- src/main/java/pingis/config/SecurityConfig.java
+++ src/main/java/pingis/config/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 "/fake/authorize", "/fake/token", "/fake/userinfo")
         .and()
         .authorizeRequests().antMatchers(
-                "/css/**", "/webjars/**", "/", "/login", "/logout",
+                "/css/**", "/webjars/**", "/", "/login", "/logout", "/img/**",
                 "/fake/authorize", "/fake/token", "/fake/userinfo")
         .permitAll()
         .anyRequest().authenticated()
