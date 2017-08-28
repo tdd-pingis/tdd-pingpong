6260cf539b495a7aced3a8cf68f4e6de89ee9b96
Heliozoa <dm89132@gmail.com>
2017-08-28 17:53:29 +0300

    Compile errors now all pass test tasks, also fixed an error in printing the results

--- src/main/java/pingis/services/sandbox/SandboxService.java
+++ src/main/java/pingis/services/sandbox/SandboxService.java
@@ -107,6 +107,7 @@ public class SandboxService {
     message.setStatus(status);
 
     if (status == ResultStatus.COMPILE_FAILED) {
+      message.setSuccess(type == TaskType.TEST);
       message.setStdout(logs.getStdoutString());
     } else if (status == ResultStatus.PASSED) {
       message.setSuccess(type == TaskType.IMPLEMENTATION);
