c951a0b87cf2dfb68f4a1d814911a8057b2f0d53
lauri <lauri334@hotmail.com>
2017-09-01 01:06:19 +0300

    forgot to include new ranks

--- src/main/java/pingis/services/Rank.java
+++ src/main/java/pingis/services/Rank.java
@@ -2,7 +2,18 @@ package pingis.services;
 
 public enum Rank {
   // TODO: Let's think these over. More ranks = more fun for the player.
-  TOMATO(0), NOOB(1), WANNABE(10), NOVICE(20), GEEK(30), APPPRENTICE(40), PROGRAMMER(50), HACKER(60), NINJA(70), WIZARD(80), SUPERGURU(90), OUTOFBOUNDSEXCEPTION(100)
+  TOMATO(0),
+  NOOB(1),
+  WANNABE(10),
+  NOVICE(20),
+  GEEK(30),
+  APPPRENTICE(40),
+  PROGRAMMER(50),
+  HACKER(60),
+  NINJA(70),
+  WIZARD(80),
+  SUPERGURU(90),
+  OUTOFBOUNDSEXCEPTION(100);
   private int limit;
 
   private Rank(int limit) {
