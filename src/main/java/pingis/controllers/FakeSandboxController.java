2d87286da715e1b3fbaec12fbb07d5e1ee3cfcd3
Heliozoa <dm89132@gmail.com>
2017-08-31 22:35:50 +0300

    Fake sandbox can now send different results

--- src/main/java/pingis/controllers/FakeSandboxController.java
+++ src/main/java/pingis/controllers/FakeSandboxController.java
@@ -1,5 +1,7 @@
 package pingis.controllers;
 
+import java.io.IOException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import pingis.entities.sandbox.RequestedStatus;
 import pingis.services.sandbox.FakeSandboxRestService;
 import pingis.services.sandbox.SubmissionResponse;
 
@@ -28,24 +32,45 @@ public class FakeSandboxController {
   @RequestMapping("/tasks.json")
   public ResponseEntity<SubmissionResponse> tasks(
         @RequestParam("token") String token,
-        @RequestParam("notify") String notify) {
+        @RequestParam("notify") String notify,
+        @RequestParam("file") MultipartFile statusAsFile) throws IOException {
 
-    logger.debug("TOKEN::::::{}", token);
-    logger.debug("NOTIFY:::::{}", notify);
+    logger.debug("Fake sandbox received request");
+
+    RequestedStatus requestedStatus;
+    try {
+      requestedStatus = getRequestedStatusFromFile(statusAsFile);
+    } catch (IllegalArgumentException ex) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
 
     //NOTE: Making STOMP messages persist makes this obsolete
     try {
       Thread.sleep(2000);
     } catch (InterruptedException ex) {
-      logger.error("Sandbox thread stopped unexpectedly");
+      logger.error("Sandbox thread stopped unexpectedly: {}", ex.getMessage());
     }
 
-    fakeSandboxRestService.postSubmissionResults(token, notify);
+    fakeSandboxRestService.postSubmissionResults(token, notify, requestedStatus);
 
     SubmissionResponse sr = new SubmissionResponse();
     sr.setStatus("ok");
 
     logger.debug("Returning response");
     return ResponseEntity.status(HttpStatus.OK).body(sr);
+  }
+
+  private RequestedStatus getRequestedStatusFromFile(
+          MultipartFile statusAsFile) throws IOException, IllegalArgumentException {
+
+    try {
+      byte[] bytes = statusAsFile.getBytes();
+      String statusName = new String(bytes);
+      return RequestedStatus.valueOf(statusName);
+
+    } catch (IllegalArgumentException ex) {
+      logger.debug("The file should be a value of RequestedStatus");
+      throw ex;
+    }
   }
 }
--- src/test/java/pingis/controllers/FakeSandboxControllerTest.java
+++ src/test/java/pingis/controllers/FakeSandboxControllerTest.java
@@ -1,11 +1,15 @@
 package pingis.controllers;
 
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static pingis.entities.sandbox.RequestedStatus.COMPILE_FAILED;
+import static pingis.entities.sandbox.RequestedStatus.TASK_PASSING;
+import static pingis.entities.sandbox.RequestedStatus.TESTS_FAILED;
+import static pingis.entities.sandbox.RequestedStatus.TESTS_PASSED;
 
 import java.util.UUID;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +21,7 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import pingis.config.OAuthProperties;
 import pingis.config.SecurityConfig;
+import pingis.entities.sandbox.RequestedStatus;
 import pingis.services.sandbox.FakeSandboxRestService;
 
 /**
@@ -36,16 +41,52 @@ public class FakeSandboxControllerTest {
   @MockBean
   private FakeSandboxRestService fakeSandboxMock;
 
-  @Test
-  public void resultStatusFailedWhenTestTaskInSubmission() throws Exception {
-    UUID uuid = UUID.randomUUID();
-    String url = "localhost";
+  private UUID uuid;
+  private String url;
 
-    mvc.perform(post("/tasks.json")
+  @Before
+  public void setup() {
+    uuid = UUID.randomUUID();
+    url = "localhost";
+  }
+
+  @Test
+  public void postsTestsPassedWhenRequested() throws Exception {
+    postRequestedStatusAndVerify(TESTS_PASSED);
+  }
+
+  @Test
+  public void postsTestsFailedWhenRequested() throws Exception {
+    postRequestedStatusAndVerify(TESTS_FAILED);
+  }
+
+  @Test
+  public void postsCompileFailedWhenRequested() throws Exception {
+    postRequestedStatusAndVerify(COMPILE_FAILED);
+  }
+
+  @Test
+  public void postsTaskPassingWhenRequested() throws Exception {
+    postRequestedStatusAndVerify(TASK_PASSING);
+  }
+
+  @Test
+  public void badRequestWithInvalidFile() throws Exception {
+    mvc.perform(multipart("/tasks.json")
+        .file("file", "any".getBytes())
+        .param("token", uuid.toString())
+        .param("notify", url))
+        .andExpect(status().isBadRequest());
+  }
+
+  private void postRequestedStatusAndVerify(RequestedStatus request) throws Exception {
+    mvc.perform(multipart("/tasks.json")
+        .file("file", request.asBytes())
         .param("token", uuid.toString())
         .param("notify", url))
         .andExpect(status().isOk());
 
-    verify(fakeSandboxMock).postSubmissionResults(eq(uuid.toString()), eq(url));
+    verify(fakeSandboxMock).postSubmissionResults(
+            uuid.toString(), url, request);
   }
 }
