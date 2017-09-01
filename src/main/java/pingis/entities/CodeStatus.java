630f9d986b1975ad15402eb55bcce6be84eb3db7
Ville Saarinen <ville.saarinen@helsinki.fi>
2017-09-01 16:08:47 +0300

    front fixes

--- src/main/java/pingis/entities/CodeStatus.java
+++ src/main/java/pingis/entities/CodeStatus.java
@@ -1,5 +1,17 @@
 package pingis.entities;
 
 public enum CodeStatus {
-  DONE, IN_PROGRESS, DROPPED
+  DONE("Done"),
+  IN_PROGRESS("In progress"),
+  DROPPED("Dropped");
+
+  private final String name;
+
+  private CodeStatus(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
 }
